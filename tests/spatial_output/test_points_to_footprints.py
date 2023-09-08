1330fcf49b7ca6d4dd552dfc3d1d53ea7932b5b1
Panos <panosatha90@gmail.com>
2023-09-08 11:05:02 +0200

    update tests and test data

--- tests/spatial_output/test_points_to_footprints.py
+++ tests/spatial_output/test_points_to_footprints.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import geopandas as gpd
 import pandas as pd
+import pytest
 
 from fiat_toolbox.spatial_output.points_to_footprint import PointsToFootprints
 
@@ -10,31 +11,60 @@ file_path = Path(__file__).parent.resolve()
 
 def test_write_footprints_event():
     # Get footprints file
-    footprints_path = file_path / "data" / "Buildings.shp"
+    footprints_path = file_path / "data" / "building_footprints.geojson"
     # Get fiat results file
-    results_path = file_path / "data" / "output_event" / "output.csv"
+    results_path = file_path / "data" / "output_event.csv"
 
     footprints = gpd.read_file(footprints_path)
     results = pd.read_csv(results_path)
 
     # Define output name
     outpath = file_path / "building_footprints_event.gpkg"
-    PointsToFootprints.write_footprint_file(footprints, results, outpath)
+    out = PointsToFootprints.write_footprint_file(footprints, results, outpath)
+
+    out_example = out["Total Damage"][out["Object ID"] == "1393_1394"].values[0]
+    in_example = (
+        results["Total Damage"][results["Object ID"] == 1393].values[0]
+        + results["Total Damage"][results["Object ID"] == 1394].values[0]
+    )
+    assert out_example == in_example
+    # Delete created files
+    outpath.unlink()
 
 
 def test_write_footprints_risk():
     # Get footprints file
-    footprints_path = file_path / "data" / "Buildings.shp"
+    footprints_path = file_path / "data" / "building_footprints.geojson"
     # Get fiat results file
-    results_path = file_path / "data" / "output_risk" / "output.csv"
+    results_path = file_path / "data" / "output_risk.csv"
 
     footprints = gpd.read_file(footprints_path)
     results = pd.read_csv(results_path)
 
     # Define output name
     outpath = file_path / "building_footprints_risk.gpkg"
-    PointsToFootprints.write_footprint_file(footprints, results, outpath)
+    out = PointsToFootprints.write_footprint_file(footprints, results, outpath)
+
+    out_example = out["Risk (EAD)"][out["Object ID"] == "1393_1394"].values[0]
+    in_example = (
+        results["Risk (EAD)"][results["Object ID"] == 1393].values[0]
+        + results["Risk (EAD)"][results["Object ID"] == 1394].values[0]
+    )
+    assert out_example == in_example
+    # Delete created files
+    outpath.unlink()
 
 
 def test_error_handling():
-    pass
+    # Get footprints file
+    footprints_path = file_path / "data" / "building_footprints.geojson"
+    # Get fiat results file
+    results_path = file_path / "data" / "output_risk.csv"
+
+    footprints = gpd.read_file(footprints_path)
+    results = pd.read_csv(results_path)
+    del results["Risk (EAD)"]
+    # Define output name
+    outpath = file_path / "building_footprints_risk.gpkg"
+    with pytest.raises(ValueError):
+        PointsToFootprints.write_footprint_file(footprints, results, outpath)
